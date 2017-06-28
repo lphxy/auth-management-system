b0d369cb519bfd685bc6df43e3cef8d9ff77b818
wanqinfeng <571716844@qq.com>
2017-06-28 11:08:31 +0800

    update cms-dao
    Signed-off-by: wanqinfeng <wanqinfeng@dvt.dvt.com>

---
+++ cms/cms-dao/src/main/java/com/wan/cms/model/CmsTag.java
@@ -0,0 +1,13 @@
+package com.wan.cms.model;
+
+public class CmsTag {
+    private Integer tagId;
+
+    public Integer getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(Integer tagId) {
+        this.tagId = tagId;
+    }
+}
