2b94ee243073babf642d6a6697b02672812a2f5e
wanqinfeng <571716844@qq.com>
2017-06-28 11:56:51 +0800

    add ehcache
    Signed-off-by: wanqinfeng <wanqinfeng@dvt.dvt.com>

--- cms/cms-service/src/test/java/com/wan/cms/service/UserServiceTest.java
+++ cms/cms-service/src/test/java/com/wan/cms/service/UserServiceTest.java
@@ -2,6 +2,9 @@ package com.wan.cms.service;
 
 import com.wan.cms.model.User;
 import com.wan.cms.model.UserVO;
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +18,8 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({
         "classpath:applicationContext.xml",
-        "classpath:applicationContext-jdbc.xml"
+        "classpath:applicationContext-jdbc.xml",
+        "classpath:applicationContext-ehcache.xml"
 })
 @TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
 public class UserServiceTest {
@@ -26,9 +30,30 @@ public class UserServiceTest {
     public void index() {
         // 自定义接口调用
         UserVO userVO = userService.selectUserWithBook(1);
-        System.out.println(userVO.getBooks().size());
+        System.out.println(null == userVO ? "null" : userVO.getBooks().size());
         // 自动生成接口调用
-        User user2 = userService.getMapper().selectByPrimaryKey(1);
-        System.out.println(user2.getNickname());
+        User user = userService.getMapper().selectByPrimaryKey(1);
+        System.out.println(null == user ? "null" :user.getNickname());
+
+        // Create a cache manager
+        final CacheManager cacheManager = CacheManager.getInstance();
+
+        // create the cache called "hello-world"
+        final Cache cache = cacheManager.getCache("ehCache");
+
+        // create a key to map the data to
+        final String key = "key";
+
+        // Create a data element
+        final Element element = new Element(key, "value");
+
+        // Put the element into the data store
+        cache.put(element);
+
+        // Retrieve the data element
+        final Element cacheElement = cache.get(key);
+
+        // Print the value
+        System.out.println(cacheElement.getObjectValue());
     }
 }
