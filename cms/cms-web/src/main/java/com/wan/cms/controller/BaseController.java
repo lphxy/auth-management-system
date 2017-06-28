7073ddecbb1c349d5f9a1ebcc75a0d22c54c0fb2
wanqinfeng <571716844@qq.com>
2017-06-28 10:45:11 +0800

    add cms-web

---
+++ cms/cms-web/src/main/java/com/wan/cms/controller/BaseController.java
@@ -0,0 +1,14 @@
+package com.wan.cms.controller;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Created by w1992wishes on 2017/6/26.
+ */
+public class BaseController {
+    public static final String RESULT = "result";
+    public static final String DATA = "data";
+    public static final String SUCCESS = "success";
+    public static final String FAILED = "failed";
+
+}
