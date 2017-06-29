7941690889c92139e499236123ff8c5b8f35f81d
wanqinfeng <571716844@qq.com>
2017-06-29 15:28:11 +0800

    mybatis generator生成的插入方法执行后，返回自增主键
    Signed-off-by: wanqinfeng <wanqinfeng@dvt.dvt.com>

---
+++ cms/cms-dao/src/main/java/com/wan/cms/mapper/CmsTagMapper.java
@@ -0,0 +1,30 @@
+package com.wan.cms.mapper;
+
+import com.wan.cms.model.CmsTag;
+import com.wan.cms.model.CmsTagExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface CmsTagMapper {
+    int countByExample(CmsTagExample example);
+
+    int deleteByExample(CmsTagExample example);
+
+    int deleteByPrimaryKey(Integer tagId);
+
+    int insert(CmsTag record);
+
+    int insertSelective(CmsTag record);
+
+    List<CmsTag> selectByExample(CmsTagExample example);
+
+    CmsTag selectByPrimaryKey(Integer tagId);
+
+    int updateByExampleSelective(@Param("record") CmsTag record, @Param("example") CmsTagExample example);
+
+    int updateByExample(@Param("record") CmsTag record, @Param("example") CmsTagExample example);
+
+    int updateByPrimaryKeySelective(CmsTag record);
+
+    int updateByPrimaryKey(CmsTag record);
+}
