0b2bc08ed7f4f90d42265ef8a11970f6b9fc685e
wanqinfeng <571716844@qq.com>
2017-06-29 11:13:57 +0800

    使用分页插件生成dao代码
    Signed-off-by: wanqinfeng <wanqinfeng@dvt.dvt.com>

---
+++ cms/cms-dao/src/main/java/com/wan/cms/mapper/CmsCategoryTagMapper.java
@@ -0,0 +1,30 @@
+package com.wan.cms.mapper;
+
+import com.wan.cms.model.CmsCategoryTag;
+import com.wan.cms.model.CmsCategoryTagExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface CmsCategoryTagMapper {
+    int countByExample(CmsCategoryTagExample example);
+
+    int deleteByExample(CmsCategoryTagExample example);
+
+    int deleteByPrimaryKey(Integer categoryTagId);
+
+    int insert(CmsCategoryTag record);
+
+    int insertSelective(CmsCategoryTag record);
+
+    List<CmsCategoryTag> selectByExample(CmsCategoryTagExample example);
+
+    CmsCategoryTag selectByPrimaryKey(Integer categoryTagId);
+
+    int updateByExampleSelective(@Param("record") CmsCategoryTag record, @Param("example") CmsCategoryTagExample example);
+
+    int updateByExample(@Param("record") CmsCategoryTag record, @Param("example") CmsCategoryTagExample example);
+
+    int updateByPrimaryKeySelective(CmsCategoryTag record);
+
+    int updateByPrimaryKey(CmsCategoryTag record);
+}
