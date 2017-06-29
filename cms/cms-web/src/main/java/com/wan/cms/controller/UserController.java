a2b3c0417bcd95695f82b164047834e1a806c7be
wanqinfeng <571716844@qq.com>
2017-06-29 11:21:11 +0800

    使用新的分页操作方法
    Signed-off-by: wanqinfeng <wanqinfeng@dvt.dvt.com>

--- cms/cms-web/src/main/java/com/wan/cms/controller/UserController.java
+++ cms/cms-web/src/main/java/com/wan/cms/controller/UserController.java
@@ -58,19 +58,19 @@ public class UserController extends BaseController {
     public String list(
             @RequestParam(required = false, defaultValue = "1") int page,
             @RequestParam(required = false, defaultValue = "20") int rows,
-            HttpServletRequest request) {
-        // 查询参数
-        String clumns = " * ";
-        String condition = " id>0 ";
-        String order = " id asc ";
-        Map<String,Object> parameters = new HashMap<String, Object>();
-        parameters.put("clumns", clumns);
-        parameters.put("condition", condition);
-        parameters.put("order", order);
-        // 创建分页对象
+            HttpServletRequest request, Model model) {
+
         UserExample userExample = new UserExample();
         userExample.createCriteria()
                 .andIdGreaterThan(0);
+        userExample.setOffset((page -1) * rows);
+        userExample.setLimit(rows);
+        userExample.setDistinct(false);
+        userExample.setOrderByClause(" id desc ");
+        List<User> users = userService.getMapper().selectByExample(userExample);
+        model.addAttribute("users", users);
+
+        // 创建分页对象
         long total = userService.getMapper().countByExample(userExample);
         Paginator paginator = new Paginator();
         paginator.setTotal(total);
@@ -79,15 +79,8 @@ public class UserController extends BaseController {
         paginator.setParam("page");
         paginator.setUrl(request.getRequestURI());
         paginator.setQuery(request.getQueryString());
-        // 调用有分页功能的方法
-        parameters.put("paginator", paginator);
-        List<User> users = userService.selectAll(parameters);
-        // 返回数据
-        request.setAttribute("users", users);
-        request.setAttribute("paginator", paginator);
+        model.addAttribute("paginator", paginator);
 
-
-        //PageHelper.startPage(1, 10);
         return "/user/list";
     }
 
@@ -226,5 +219,4 @@ public class UserController extends BaseController {
     public Object ajax(@PathVariable int id) {
         return userService.getMapper().selectByPrimaryKey(id);
     }
-
 }
