1d9e7220834369decb08a172b2e1998f4b7ab9b6
wanqinfeng <571716844@qq.com>
2017-07-10 21:56:48 +0800

    启动spring task
    Signed-off-by: wanqinfeng <571716844@qq.com>

---
+++ cms/cms-web/src/main/java/com/wan/cms/task/TestTask.java
@@ -0,0 +1,8 @@
+package com.wan.cms.task;
+
+/**
+ * Created by wanqinfeng on 2017/7/10.
+ */
+public interface TestTask {
+    void test();
+}
---
+++ cms/cms-web/src/main/java/com/wan/cms/task/TestTaskImpl.java
@@ -0,0 +1,16 @@
+package com.wan.cms.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by wanqinfeng on 2017/7/10.
+ */
+@Component
+public class TestTaskImpl implements TestTask {
+    @Scheduled(cron = "0 0/1 * * * ?")
+    @Override
+    public void test() {
+        System.out.println("Task");
+    }
+}
