35576c90c9d4e2fda003e14091cfb3ecf5870222
wanqinfeng <571716844@qq.com>
2017-07-20 19:32:00 +0800

    增加activeMQ支持
    Signed-off-by: wanqinfeng <571716844@qq.com>

---
+++ cms/cms-web/src/main/java/com/wan/cms/controller/ActiveMQController.java
@@ -0,0 +1,34 @@
+package com.wan.cms.controller;
+
+import com.wan.common.util.JmsUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.jms.Destination;
+
+/**
+ * Created by w1992wishes on 2017/7/20.
+ */
+@Controller
+@RequestMapping("/activemq")
+public class ActiveMQController extends BaseController {
+
+    @Autowired
+    JmsTemplate jmsTemplate;
+
+    @Autowired
+    Destination defaultQueueDestination;
+
+    @RequestMapping("/send")
+    @ResponseBody
+    public Object send() {
+        for (int i = 0; i < 1000; i ++) {
+            JmsUtil.sendMessage(jmsTemplate, defaultQueueDestination, "消息" + i);
+        }
+        return "success";
+    }
+
+}
---
+++ cms/cms-web/src/main/java/com/wan/cms/jms/defaultQueueMessageListener.java
@@ -0,0 +1,37 @@
+package com.wan.cms.jms;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ * Created by w1992wishes on 2017/7/20.
+ */
+public class defaultQueueMessageListener implements MessageListener {
+
+    private static Logger logger = LoggerFactory.getLogger(defaultQueueMessageListener.class);
+
+    @Autowired
+    ThreadPoolTaskExecutor threadPoolTaskExecutor;
+
+    public void onMessage(final Message message) {
+        // 使用线程池多线程处理
+        threadPoolTaskExecutor.execute(new Runnable() {
+            public void run() {
+                TextMessage textMessage = (TextMessage) message;
+                try {
+                    logger.info("cms-web接收到：{}", textMessage.getText());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+}
