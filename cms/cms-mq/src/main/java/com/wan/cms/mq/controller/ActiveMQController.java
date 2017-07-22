3ad48b4c776b75b03adcccef502c24b23758a207
wanqinfeng <571716844@qq.com>
2017-07-22 15:30:09 +0800

    多线程发队列消息
    Signed-off-by: wanqinfeng <571716844@qq.com>

--- cms/cms-mq/src/main/java/com/wan/cms/mq/controller/ActiveMQController.java
+++ cms/cms-mq/src/main/java/com/wan/cms/mq/controller/ActiveMQController.java
@@ -1,9 +1,12 @@
 package com.wan.cms.mq.controller;
 
 import com.wan.common.util.JmsUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,24 +21,38 @@ import javax.jms.Destination;
 @RequestMapping("/activemq")
 public class ActiveMQController{
 
-    @PostConstruct
-    public void init(){
-        System.out.println("--------------------------");
-    }
+    Logger logger = LoggerFactory.getLogger(ActiveMQController.class);
 
     @Autowired
     @Qualifier("jmsQueueTemplate")
     JmsTemplate jmsTemplate;
-
     @Autowired
     Destination defaultQueueDestination;
+    @Autowired
+    ThreadPoolTaskExecutor threadPoolTaskExecutor;
 
-    @RequestMapping("/send")
+    @RequestMapping("/send1")
     @ResponseBody
-    public Object send() {
+    public Object send1() {
+        long start = System.currentTimeMillis();
         for (int i = 0; i < 1000; i ++) {
             JmsUtil.sendMessage(jmsTemplate, defaultQueueDestination, "消息" + i);
         }
+        long end = System.currentTimeMillis();
+        logger.info("发送消息消耗时间" + (end -start));
+        return "success";
+    }
+
+    @RequestMapping("/send2")
+    @ResponseBody
+    public Object send2() {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1000; i ++) {
+            long time = System.currentTimeMillis();
+            threadPoolTaskExecutor.execute(() -> JmsUtil.sendMessage(jmsTemplate, defaultQueueDestination, "消息" + time));
+        }
+        long end = System.currentTimeMillis();
+        logger.info("发送消息消耗时间" + (end -start));
         return "success";
     }
 
