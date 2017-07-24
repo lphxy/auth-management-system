a6afbc979d6d57424652dbabe540e553fa563506
wanqinfeng <571716844@qq.com>
2017-07-24 19:39:25 +0800

    解决运行redis cache错误
    Signed-off-by: wanqinfeng <571716844@qq.com>

--- common/src/main/java/com/wan/common/util/RedisUtil.java
+++ common/src/main/java/com/wan/common/util/RedisUtil.java
@@ -64,8 +64,7 @@ public class RedisUtil {
             config.setMaxIdle(MAX_IDLE);
             config.setMaxWaitMillis(MAX_WAIT);
             config.setTestOnBorrow(TEST_ON_BORROW);
-            //jedisPool = new JedisPool(config, IP.split(",")[0], PORT, TIMEOUT, PASSWORD);
-            jedisPool = new JedisPool(config, IP.split(",")[0], PORT, TIMEOUT);
+            jedisPool = new JedisPool(config, IP.split(",")[0], PORT, TIMEOUT, PASSWORD);
         } catch (Exception e) {
             logger.error("First create JedisPool error : " + e);
             try {
@@ -76,7 +75,7 @@ public class RedisUtil {
                 config.setMaxWaitMillis(MAX_WAIT);
                 config.setTestOnBorrow(TEST_ON_BORROW);
                 //jedisPool = new JedisPool(config, IP.split(",")[1], PORT, TIMEOUT, PASSWORD);
-                jedisPool = new JedisPool(config, IP.split(",")[1], PORT, TIMEOUT);
+                jedisPool = new JedisPool(config, IP.split(",")[1], PORT, TIMEOUT, PASSWORD);
             } catch (Exception e2) {
                 logger.error("Second create JedisPool error : " + e2);
             }
