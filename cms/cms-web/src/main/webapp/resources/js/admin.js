df9f44aa09ed87d037ab0bd89002a8cb665b795f
wanqinfeng <571716844@qq.com>
2017-07-27 20:56:40 +0800

    增加选项卡
    Signed-off-by: wanqinfeng <571716844@qq.com>

--- cms/cms-web/src/main/webapp/resources/js/admin.js
+++ cms/cms-web/src/main/webapp/resources/js/admin.js
@@ -1,10 +1,35 @@
-function changeFrameHeight(ifm) {
-    ifm.height = document.documentElement.clientHeight - 118;
-}
-function resizeFrameHeight() {
-    $('.tab_iframe').css('height', document.documentElement.clientHeight - 118);
-    $('md-tab-content').css('left', '0');
-}
-window.onresize = function(){
-    resizeFrameHeight();
-}
+$(function() {
+    // 侧边栏操作按钮
+    $('#guide').click(function() {
+        $(this).toggleClass('toggled');
+        $('#sidebar').toggleClass('toggled');
+    });
+    // 侧边栏二级菜单
+    $('.sub-menu a').click(function() {
+        $(this).next().slideToggle(200);
+        $(this).parent().toggleClass('toggled');
+    });
+    // 个人资料
+    $('.s-profile a').click(function() {
+        $(this).next().slideToggle(200);
+        $(this).parent().toggleClass('toggled');
+    });
+    // Waves初始化
+    Waves.displayEffect();
+    // 滚动条初始化
+    $('#sidebar').mCustomScrollbar({
+        theme: 'minimal-dark',
+        scrollInertia: 100,
+        axis: 'yx',
+        mouseWheel: {
+            enable: true,
+            axis: 'y',
+            preventDefault: true
+        }
+    });
+    // 选项卡
+    $('.content_tab li').click(function() {
+        $('.content_tab li').removeClass('cur');
+        $(this).addClass('cur');
+    });
+});
