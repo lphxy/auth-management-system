c7ba35130ec858c557dd6ba48a96e537ae0638c0
wanqinfeng <571716844@qq.com>
2017-07-31 19:02:08 +0800

    update static resource
    Signed-off-by: wanqinfeng <571716844@qq.com>

--- cms/cms-web/src/main/webapp/resources/js/admin.js
+++ cms/cms-web/src/main/webapp/resources/js/admin.js
@@ -1,16 +1,16 @@
-$(function() {
+$(function () {
     // 侧边栏操作按钮
-    $('#guide').click(function() {
+    $('#guide').click(function () {
         $(this).toggleClass('toggled');
         $('#sidebar').toggleClass('toggled');
     });
     // 侧边栏二级菜单
-    $('.sub-menu a').click(function() {
+    $('.sub-menu a').click(function () {
         $(this).next().slideToggle(200);
         $(this).parent().toggleClass('toggled');
     });
     // 个人资料
-    $('.s-profile a').click(function() {
+    $('.s-profile a').click(function () {
         $(this).next().slideToggle(200);
         $(this).parent().toggleClass('toggled');
     });
@@ -27,98 +27,6 @@ $(function() {
             preventDefault: true
         }
     });
-    // 选项卡
-    $(document).on('click', '.content_tab li', function() {
-        // 切换选项卡
-        $('.content_tab li').removeClass('cur');
-        $(this).addClass('cur');
-        // 切换iframe
-        $('.iframe').removeClass('cur');
-        $('#iframe_' + $(this).data('index')).addClass('cur');
-    });
-    // 控制选项卡滚动位置
-    $('.tab_left>a').click(function() {
-        $('.content_tab>ul').animate({scrollLeft: $('.content_tab>ul').scrollLeft() - 300}, 200);
-        if ($('.content_tab>ul').scrollLeft() == 0) {
-            $('.tab_left>a').removeClass('active');
-        } else {
-            $('.tab_left>a').addClass('active');
-        }
-    });
-    $('.tab_right>a').click(function() {
-        $('.content_tab>ul').animate({scrollLeft: $('.content_tab>ul').scrollLeft() + 300}, 200);
-        if ($('.content_tab>ul').scrollLeft() == 0) {
-            $('.tab_left>a').removeClass('active');
-        } else {
-            $('.tab_left>a').addClass('active');
-        }
-    });
-    // 初始化箭头状态
-
-    // 选项卡右键菜单
-    var menu = new BootstrapMenu('.tabs li', {
-        fetchElementData: function(item) {
-            return item;
-        },
-        actionsGroups: [
-            ['close', 'closeOther', 'closeAll'],
-            ['closeRight', 'closeLeft'],
-            ['refresh'],
-        ],
-        actions: {
-            close: {
-                name: '关闭',
-                iconClass: 'zmdi zmdi-close',
-                onClick: function(item) {
-                    Tab.closeTab($(item));
-                }
-            },
-            closeOther: {
-                name: '关闭其他',
-                iconClass: 'zmdi zmdi-arrow-split',
-                onClick: function(item) {
-                    var index = $(item).data('index');
-                    $('.content_tab li').each(function() {
-                        if ($(this).data('index') != index) {
-                            Tab.closeTab($(this));
-                        }
-                    });
-                }
-            },
-            closeAll: {
-                name: '关闭全部',
-                iconClass: 'zmdi zmdi-swap',
-                onClick: function() {
-                    $('.content_tab li').each(function() {
-                        Tab.closeTab($(this));
-                    });
-                }
-            },
-            closeRight: {
-                name: '关闭右侧所有',
-                iconClass: 'zmdi zmdi-arrow-right',
-                onClick: function() {
-
-                }
-            },
-            closeLeft: {
-                name: '关闭左侧所有',
-                iconClass: 'zmdi zmdi-arrow-left',
-                onClick: function() {
-
-                }
-            },
-            refresh: {
-                name: '刷新',
-                iconClass: 'zmdi zmdi-refresh',
-                onClick: function(item) {
-                    var index = $(item).data('index');
-                    var $iframe = $('#iframe_' + index).find('iframe');
-                    $iframe.attr('src', $iframe.attr('src'));
-                }
-            }
-        }
-    });
 });
 // iframe高度自适应
 function changeFrameHeight(ifm) {
@@ -128,32 +36,158 @@ function resizeFrameHeight() {
     $('.tab_iframe').css('height', document.documentElement.clientHeight - 118);
     $('md-tab-content').css('left', '0');
 }
-window.onresize = function(){
+window.onresize = function () {
     resizeFrameHeight();
+    initScrollShow();
+    initScrollState();
 }
-// 选项卡操作
+
+// ========== 选项卡操作 ==========
+$(function () {
+    // 选项卡点击
+    $(document).on('click', '.content_tab li', function () {
+        // 切换选项卡
+        $('.content_tab li').removeClass('cur');
+        $(this).addClass('cur');
+        // 切换iframe
+        $('.iframe').removeClass('cur');
+        $('#iframe_' + $(this).data('index')).addClass('cur');
+        var marginLeft = ($('#tabs').css('marginLeft').replace('px', ''));
+        // 滚动到可视区域:在左侧
+        if ($(this).position().left < marginLeft) {
+            var left = $('.content_tab>ul').scrollLeft() + $(this).position().left - marginLeft;
+            $('.content_tab>ul').animate({scrollLeft: left}, 200, function () {
+                initScrollState();
+            });
+        }
+        // 滚动到可视区域:在右侧
+        if (($('.content_tab>ul').scrollLeft() + document.getElementById('tabs').clientWidth) < ($(this).position().left + $(this).width())) {
+            var left = ($(this).position().left + $(this).width()) - document.getElementById('tabs').clientWidth;
+            $('.content_tab>ul').animate({scrollLeft: left}, 200, function () {
+                initScrollState();
+            });
+        }
+    });
+    // 控制选项卡滚动位置
+    $('.tab_left>a').click(function () {
+        $('.content_tab>ul').animate({scrollLeft: $('.content_tab>ul').scrollLeft() - 300}, 200, function () {
+            initScrollState();
+        });
+    });
+    // 向右箭头
+    $('.tab_right>a').click(function () {
+        $('.content_tab>ul').animate({scrollLeft: $('.content_tab>ul').scrollLeft() + 300}, 200, function () {
+            initScrollState();
+        });
+    });
+    // 初始化箭头状态
+
+    // 选项卡右键菜单
+    var menu = new BootstrapMenu('.tabs li', {
+        fetchElementData: function (item) {
+            return item;
+        },
+        actionsGroups: [
+            ['close', 'refresh'],
+            ['closeOther', 'closeAll'],
+            ['closeRight', 'closeLeft']
+        ],
+        actions: {
+            close: {
+                name: '关闭',
+                iconClass: 'zmdi zmdi-close',
+                onClick: function (item) {
+                    Tab.closeTab($(item));
+                }
+            },
+            closeOther: {
+                name: '关闭其他',
+                iconClass: 'zmdi zmdi-arrow-split',
+                onClick: function (item) {
+                    var index = $(item).data('index');
+                    $('.content_tab li').each(function () {
+                        if ($(this).data('index') != index) {
+                            Tab.closeTab($(this));
+                        }
+                    });
+                }
+            },
+            closeAll: {
+                name: '关闭全部',
+                iconClass: 'zmdi zmdi-swap',
+                onClick: function () {
+                    $('.content_tab li').each(function () {
+                        Tab.closeTab($(this));
+                    });
+                }
+            },
+            closeRight: {
+                name: '关闭右侧所有',
+                iconClass: 'zmdi zmdi-arrow-right',
+                onClick: function (item) {
+                    var index = $(item).data('index');
+                    $($('.content_tab li').toArray().reverse()).each(function () {
+                        if ($(this).data('index') != index) {
+                            Tab.closeTab($(this));
+                        } else {
+                            return false;
+                        }
+                    });
+                }
+            },
+            closeLeft: {
+                name: '关闭左侧所有',
+                iconClass: 'zmdi zmdi-arrow-left',
+                onClick: function (item) {
+                    var index = $(item).data('index');
+                    $('.content_tab li').each(function () {
+                        if ($(this).data('index') != index) {
+                            Tab.closeTab($(this));
+                        } else {
+                            return false;
+                        }
+                    });
+                }
+            },
+            refresh: {
+                name: '刷新',
+                iconClass: 'zmdi zmdi-refresh',
+                onClick: function (item) {
+                    var index = $(item).data('index');
+                    var $iframe = $('#iframe_' + index).find('iframe');
+                    $iframe.attr('src', $iframe.attr('src'));
+                }
+            }
+        }
+    });
+});
+// 选项卡对象
 var Tab = {
-    addTab: function(title, url) {
+    addTab: function (title, url) {
         var index = url.replace('.', '_');
         // 如果存在选项卡，则激活，否则创建新选项卡
         if ($('#tab_' + index).length == 0) {
             // 添加选项卡
             $('.content_tab li').removeClass('cur');
-            var tab = '<li id="tab_' + index +'" data-index="' + index + '" class="cur"><a class="waves-effect waves-light" href="javascript:;">' + title + '</a></li>';//<i class="zmdi zmdi-close"></i><
+            var tab = '<li id="tab_' + index + '" data-index="' + index + '" class="cur"><a class="waves-effect waves-light" href="javascript:;">' + title + '</a></li>';//<i class="zmdi zmdi-close"></i><
             $('.content_tab>ul').append(tab);
             // 添加iframe
             $('.iframe').removeClass('cur');
             var iframe = '<div id="iframe_' + index + '" class="iframe cur"><iframe class="tab_iframe" src="' + url + '" width="100%" frameborder="0" scrolling="auto" onload="changeFrameHeight(this)"></iframe></div>';
             $('.content_main').append(iframe);
+            initScrollShow();
+            $('.content_tab>ul').animate({scrollLeft: document.getElementById('tabs').scrollWidth - document.getElementById('tabs').clientWidth}, 200, function () {
+                initScrollState();
+            });
         } else {
             $('#tab_' + index).click();
         }
     },
-    closeTab: function($item) {
+    closeTab: function ($item) {
         var closeable = $item.data('closeable');
         if (closeable != false) {
             // 如果当前时激活状态则关闭后激活左边选项卡
-            if($item.hasClass('cur')) {
+            if ($item.hasClass('cur')) {
                 $item.prev().click();
             }
             // 关闭当前选项卡
@@ -161,5 +195,25 @@ var Tab = {
             $('#iframe_' + index).remove();
             $item.remove();
         }
+        initScrollShow();
+    }
+}
+function initScrollShow() {
+    if (document.getElementById('tabs').scrollWidth > document.getElementById('tabs').clientWidth) {
+        $('.content_tab').addClass('scroll');
+    } else {
+        $('.content_tab').removeClass('scroll');
+    }
+}
+function initScrollState() {
+    if ($('.content_tab>ul').scrollLeft() == 0) {
+        $('.tab_left>a').removeClass('active');
+    } else {
+        $('.tab_left>a').addClass('active');
+    }
+    if (($('.content_tab>ul').scrollLeft() + document.getElementById('tabs').clientWidth) >= document.getElementById('tabs').scrollWidth) {
+        $('.tab_right>a').removeClass('active');
+    } else {
+        $('.tab_right>a').addClass('active');
     }
 }
---
+++ common/src/main/java/com/wan/common/util/StringUtil.java
@@ -0,0 +1,66 @@
+package com.wan.common.util;
+
+/**
+ * Created by w1992wishes on 2017/7/31.
+ */
+public class StringUtil {
+    /**
+     * object转String
+     * @param object
+     * @return
+     */
+    public static String getString(Object object) {
+        return getString(object, "");
+    }
+
+    public static String getString(Object object, String defaultValue) {
+        if (null == object) {
+            return defaultValue;
+        }
+        try {
+            return object.toString();
+        } catch (Exception e) {
+            return defaultValue;
+        }
+    }
+
+    /**
+     * object转Integer
+     * @param object
+     * @return
+     */
+    public static int getInt(Object object) {
+        return getInt(object, -1);
+    }
+
+    public static int getInt(Object object, Integer defaultValue) {
+        if (null == object) {
+            return defaultValue;
+        }
+        try {
+            return Integer.parseInt(object.toString());
+        } catch (Exception e) {
+            return defaultValue;
+        }
+    }
+
+    /**
+     * object转Boolean
+     * @param object
+     * @return
+     */
+    public static boolean getBoolean(Object object) {
+        return getBoolean(object, false);
+    }
+
+    public static boolean getBoolean(Object object, Boolean defaultValue) {
+        if (null == object) {
+            return defaultValue;
+        }
+        try {
+            return Boolean.parseBoolean(object.toString());
+        } catch (Exception e) {
+            return defaultValue;
+        }
+    }
+}
