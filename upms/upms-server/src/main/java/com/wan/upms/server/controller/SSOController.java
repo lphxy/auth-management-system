d742a597311416408b66ef8215a2f4df81950285
wanqinfeng <571716844@qq.com>
2017-08-03 20:19:23 +0800

    增加两个单点登录测试app
    Signed-off-by: wanqinfeng <571716844@qq.com>

--- upms/upms-server/src/main/java/com/wan/upms/server/controller/SSOController.java
+++ upms/upms-server/src/main/java/com/wan/upms/server/controller/SSOController.java
@@ -1,5 +1,6 @@
 package com.wan.upms.server.controller;
 
+import com.wan.common.util.RedisUtil;
 import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
@@ -7,10 +8,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -21,7 +25,15 @@ import java.util.UUID;
 public class SSOController {
 
     private static Logger logger = LoggerFactory.getLogger(SSOController.class);
-
+    private static List<String> apps = new ArrayList<>();
+    {
+        apps.add("cms-job");
+        apps.add("cms-admin");
+        apps.add("cms-web");
+        apps.add("upms-app1");
+        apps.add("upms-app2");
+        apps.add("upms-server");
+    }
     /**
      * 认证中心首页
      * @return
@@ -32,16 +44,19 @@ public class SSOController {
 
         String system_name = request.getParameter("system_name");
         String backurl = request.getParameter("backurl");
-        if (StringUtils.isEmpty(system_name) || !system_name.equals("wan-cms-server")) {
+
+        //判断请求认证系统是否注册
+        if (StringUtils.isEmpty(system_name) || !apps.contains(system_name)) {
             logger.info("未注册的系统：{}", system_name);
             return "/404";
         }
+        //判断是否存在全局会话
         // 未登录
-        if (null == session.getAttribute("isLogin")) {
+        if (StringUtils.isEmpty(RedisUtil.get(session.getId() + "_token"))) {
             return "redirect:/sso/login?backurl=" + URLEncoder.encode(backurl, "utf-8");
         }
         // 已登录
-        String token = ObjectUtils.toString(session.getAttribute(session.getId()));
+        String token = RedisUtil.get(session.getId() + "_token");
         String redirectUrl = backurl;
         if (backurl.contains("?")) {
             redirectUrl += "&token=" + token;
@@ -67,6 +82,8 @@ public class SSOController {
      */
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public String login(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+
         String backurl = request.getParameter("backurl");
         String username = request.getParameter("username");
         String password = request.getParameter("password");
@@ -79,11 +96,10 @@ public class SSOController {
             return "/404";
         }
         // 默认验证帐号密码正确，创建token
-        HttpSession session = request.getSession();
-        logger.info("子系统sessionId：{}", session.getId());
-        String token = UUID.randomUUID().toString().replace("-", "");
-        session.setAttribute("isLogin", true);
-        session.setAttribute(session.getId(), token);
+        String token = UUID.randomUUID().toString();
+        RedisUtil.set(session.getId() + "_token", token, 2*60*60);
+        RedisUtil.set(token, token, 2*60*60);
+        //回调子系统
         String redirectUrl = backurl;
         if (backurl.contains("?")) {
             redirectUrl += "&token=" + token;
@@ -94,4 +110,31 @@ public class SSOController {
         return "redirect:" + redirectUrl;
     }
 
+    /**
+     * 校验token
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/token", method = RequestMethod.GET)
+    @ResponseBody
+    public String token(HttpServletRequest request){
+        String tokenParam = request.getParameter("token");
+        String token = RedisUtil.get(tokenParam);
+        if (StringUtils.isEmpty(tokenParam) || !tokenParam.equals(token)){
+            return "failed";
+        }
+        return "success";
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout(HttpServletRequest request){
+        HttpSession session = request.getSession();
+
+        //清楚全局会话
+        String token = RedisUtil.get(session.getId() + "_token");
+        RedisUtil.remove(session.getId() + "_token");
+        RedisUtil.remove(token);
+        //通知该token的子系统退出登录
+        return "/sso/login";
+    }
 }
