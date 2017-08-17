f08d515279171c544dbd19eab590bef42f57be60
wanqinfeng <571716844@qq.com>
2017-08-17 17:26:41 +0800

    提取BaseService
    Signed-off-by: wanqinfeng <571716844@qq.com>

--- upms/upms-rpc-api/src/main/java/com/wan/upms/rpc/api/UpmsSystemService.java
+++ upms/upms-rpc-api/src/main/java/com/wan/upms/rpc/api/UpmsSystemService.java
@@ -4,6 +4,8 @@ package com.wan.upms.rpc.api;
  * Created by w1992wishes on 2017/8/14.
  */
 
+import com.wan.common.base.BaseService;
+import com.wan.upms.dao.mapper.UpmsSystemMapper;
 import com.wan.upms.dao.model.UpmsSystem;
 import com.wan.upms.dao.model.UpmsSystemExample;
 
@@ -11,6 +13,6 @@ import com.wan.upms.dao.model.UpmsSystemExample;
  * 系统service接口
  * Created by shuzheng on 2016/12/18.
  */
-public interface UpmsSystemService extends BaseService<UpmsSystem, UpmsSystemExample> {
+public interface UpmsSystemService extends BaseService<UpmsSystemMapper, UpmsSystem, UpmsSystemExample> {
 
 }
--- upms/upms-rpc-server/src/main/java/com/wan/upms/rpc/server/impl/UpmsSystemServiceImpl.java
+++ upms/upms-rpc-server/src/main/java/com/wan/upms/rpc/server/impl/UpmsSystemServiceImpl.java
@@ -1,5 +1,6 @@
 package com.wan.upms.rpc.server.impl;
 
+import com.wan.common.base.BaseServiceImpl;
 import com.wan.upms.dao.mapper.UpmsSystemMapper;
 import com.wan.upms.dao.model.UpmsSystem;
 import com.wan.upms.dao.model.UpmsSystemExample;
@@ -17,104 +18,11 @@ import java.util.List;
  * <p>
  * Created by w1992wishes on 2017/8/14.
  */
-public class UpmsSystemServiceImpl implements UpmsSystemService {
+public class UpmsSystemServiceImpl extends BaseServiceImpl<UpmsSystemMapper, UpmsSystem, UpmsSystemExample> implements UpmsSystemService {
 
     private Logger logger = LoggerFactory.getLogger(UpmsSystemServiceImpl.class);
 
     @Autowired
-    private UpmsSystemMapper upmsSystemMapper;
-
-    @Override
-    public int countByExample(UpmsSystemExample upmsSystemExample) {
-        return upmsSystemMapper.countByExample(upmsSystemExample);
-    }
-
-    @Override
-    public int deleteByExample(UpmsSystemExample upmsSystemExample) {
-        return upmsSystemMapper.deleteByExample(upmsSystemExample);
-    }
-
-    @Override
-    public int deleteByPrimaryKey(Integer id) {
-        return upmsSystemMapper.deleteByPrimaryKey(id);
-    }
-
-    @Override
-    public int insert(UpmsSystem upmsSystem) {
-        return upmsSystemMapper.insert(upmsSystem);
-    }
-
-    @Override
-    public int insertSelective(UpmsSystem upmsSystem) {
-        return upmsSystemMapper.insertSelective(upmsSystem);
-    }
-
-    @Override
-    public List<UpmsSystem> selectByExampleWithBLOBs(UpmsSystemExample upmsSystemExample) {
-        return upmsSystemMapper.selectByExample(upmsSystemExample);
-    }
-
-    @Override
-    public List<UpmsSystem> selectByExample(UpmsSystemExample upmsSystemExample) {
-        return upmsSystemMapper.selectByExample(upmsSystemExample);
-    }
-
-    @Override
-    public UpmsSystem selectByPrimaryKey(Integer id) {
-        return upmsSystemMapper.selectByPrimaryKey(id);
-    }
-
-    @Override
-    public int updateByExampleSelective(@Param("record") UpmsSystem upmsSystem, @Param("example") UpmsSystemExample upmsSystemExample) {
-        return upmsSystemMapper.updateByExampleSelective(upmsSystem, upmsSystemExample);
-    }
-
-    @Override
-    public int updateByExampleWithBLOBs(@Param("record") UpmsSystem upmsSystem, @Param("example") UpmsSystemExample upmsSystemExample) {
-        return upmsSystemMapper.updateByExample(upmsSystem, upmsSystemExample);
-    }
-
-    @Override
-    public int updateByExample(@Param("record") UpmsSystem upmsSystem, @Param("example") UpmsSystemExample upmsSystemExample) {
-        return upmsSystemMapper.updateByExample(upmsSystem, upmsSystemExample);
-    }
-
-    @Override
-    public int updateByPrimaryKeySelective(UpmsSystem upmsSystem) {
-        return upmsSystemMapper.updateByPrimaryKeySelective(upmsSystem);
-    }
-
-    @Override
-    public int updateByPrimaryKeyWithBLOBs(UpmsSystem upmsSystem) {
-        return upmsSystemMapper.updateByPrimaryKey(upmsSystem);
-    }
-
-    @Override
-    public int updateByPrimaryKey(UpmsSystem upmsSystem) {
-        return upmsSystemMapper.updateByPrimaryKey(upmsSystem);
-    }
-
-    // 批量删除
-    @Override
-    public int deleteByPrimaryKeys(String ids) {
-        logger.info("批量删除：ids={}", ids);
-        if (StringUtils.isBlank(ids)) {
-            return 0;
-        }
-        String[] idArray = ids.split("-");
-        int count = 0;
-        for (String id : idArray) {
-            if (StringUtils.isBlank(id)) {
-                continue;
-            }
-            try {
-                count += upmsSystemMapper.deleteByPrimaryKey(Integer.parseInt(id));
-            } catch (Exception e) {
-                e.printStackTrace();
-                return 0;
-            }
-        }
-        return count;
-    }
+    UpmsSystemMapper upmsSystemMapper;
 
 }
