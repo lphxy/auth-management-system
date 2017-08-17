6d18a652e95e1f2924c73d0d96072aaca66ccbeb
wanqinfeng <571716844@qq.com>
2017-08-17 17:32:20 +0800

    SpringContextUtil增加根据类型获取bean方法
    Signed-off-by: wanqinfeng <571716844@qq.com>

--- common/src/main/java/com/wan/common/util/SpringContextUtil.java
+++ common/src/main/java/com/wan/common/util/SpringContextUtil.java
@@ -4,6 +4,8 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+import java.util.Map;
+
 /**
  * spring 上下文工具类
  *
@@ -23,12 +25,31 @@ public class SpringContextUtil implements ApplicationContextAware {
         super();
     }
 
-    public static Object getBean(String name) {
-        return context.getBean(name);
-    }
-
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         context = applicationContext;
+    }
+
+    /**
+     * 根据名称获取bean
+     * @param beanName
+     * @return
+     */
+    public static Object getBean(String beanName) {
+        return context.getBean(beanName);
+    }
+
+    /**
+     * 根据类型获取bean
+     * @param clazz
+     * @return
+     */
+    public static <T> T getBean(Class<T> clazz) {
+        T t = null;
+        Map<String, T> map = context.getBeansOfType(clazz);
+        for (Map.Entry<String, T> entry : map.entrySet()) {
+            t = entry.getValue();
+        }
+        return t;
     }
 }
