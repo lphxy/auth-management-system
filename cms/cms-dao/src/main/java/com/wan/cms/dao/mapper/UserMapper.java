136a941bcfc8c06532fc208e9192b329b591e096
wanqinfeng <571716844@qq.com>
2017-08-17 19:17:43 +0800

    cms-dao重新生成

---
+++ cms/cms-dao/src/main/java/com/wan/cms/dao/mapper/UserMapper.java
@@ -0,0 +1,36 @@
+package com.wan.cms.dao.mapper;
+
+import com.wan.cms.dao.model.User;
+import com.wan.cms.dao.model.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    int countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExampleWithBLOBs(UserExample example);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Integer userId);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKeyWithBLOBs(User record);
+
+    int updateByPrimaryKey(User record);
+}
