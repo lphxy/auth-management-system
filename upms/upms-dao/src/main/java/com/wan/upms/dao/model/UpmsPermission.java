0ec0df941819b1612424627b5d7e84c3535d0b62
wanqinfeng <571716844@qq.com>
2017-08-17 17:14:58 +0800

    update upms
    Signed-off-by: wanqinfeng <571716844@qq.com>

---
+++ upms/upms-dao/src/main/java/com/wan/upms/dao/model/UpmsPermission.java
@@ -0,0 +1,64 @@
+package com.wan.upms.dao.model;
+
+import java.io.Serializable;
+
+public class UpmsPermission implements Serializable {
+    private Integer permissionId;
+
+    private Integer systemId;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getPermissionId() {
+        return permissionId;
+    }
+
+    public void setPermissionId(Integer permissionId) {
+        this.permissionId = permissionId;
+    }
+
+    public Integer getSystemId() {
+        return systemId;
+    }
+
+    public void setSystemId(Integer systemId) {
+        this.systemId = systemId;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", permissionId=").append(permissionId);
+        sb.append(", systemId=").append(systemId);
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        UpmsPermission other = (UpmsPermission) that;
+        return (this.getPermissionId() == null ? other.getPermissionId() == null : this.getPermissionId().equals(other.getPermissionId()))
+            && (this.getSystemId() == null ? other.getSystemId() == null : this.getSystemId().equals(other.getSystemId()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getPermissionId() == null) ? 0 : getPermissionId().hashCode());
+        result = prime * result + ((getSystemId() == null) ? 0 : getSystemId().hashCode());
+        return result;
+    }
+}
