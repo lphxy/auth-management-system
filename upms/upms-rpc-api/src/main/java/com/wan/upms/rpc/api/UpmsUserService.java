69f7a71b842c4a7d3350335bb2771f2335b13aa5
wanqinfeng <571716844@qq.com>
2017-08-24 19:44:46 +0800

    add UpmsUserService.java
    Signed-off-by: wanqinfeng <571716844@qq.com>

---
+++ upms/upms-rpc-api/src/main/java/com/wan/upms/rpc/api/UpmsUserService.java
@@ -0,0 +1,13 @@
+package com.wan.upms.rpc.api;
+
+import com.wan.common.base.BaseService;
+import com.wan.upms.dao.model.UpmsUser;
+import com.wan.upms.dao.model.UpmsUserExample;
+
+/**
+ * 用户service接口
+ *
+ * Created by w1992wishes on 2017/8/24.
+ */
+public interface UpmsUserService extends BaseService<UpmsUser, UpmsUserExample> {
+}
