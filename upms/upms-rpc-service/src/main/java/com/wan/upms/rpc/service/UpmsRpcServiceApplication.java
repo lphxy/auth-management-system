2f1bd31adbaef1001ea38c7f1b1d68bdacf2b176
wanqinfeng <571716844@qq.com>
2017-08-28 14:16:46 +0800

    解决报错
    Signed-off-by: wanqinfeng <571716844@qq.com>

--- upms/upms-rpc-service/src/main/java/com/wan/upms/rpc/service/UpmsRpcServiceApplication.java
+++ upms/upms-rpc-service/src/main/java/com/wan/upms/rpc/service/UpmsRpcServiceApplication.java
@@ -1,15 +1,13 @@
-package com.wan.cms.rpc.service;
+package com.wan.upms.rpc.service;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * 服务启动类
- *
  * Created by w1992wishes on 2017/8/28.
  */
-public class CmsRpcServiceApplication {
+public class UpmsRpcServiceApplication {
     public static void main(String[] args) {
         new ClassPathXmlApplicationContext("classpath*:applicationContext*.xml");
-        System.out.println(">>>>> cms-rpc-service 启动完成 <<<<<");
+        System.out.println(">>>>> upms-rpc-service 启动完成 <<<<<");
     }
 }
