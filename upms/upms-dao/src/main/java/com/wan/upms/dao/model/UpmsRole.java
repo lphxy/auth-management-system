4c8c7290fdadea711786b6dbb872c78a4f748b2c
wanqinfeng <571716844@qq.com>
2017-09-04 19:12:24 +0800

    startup
    Signed-off-by: wanqinfeng <571716844@qq.com>

---
+++ upms/upms-dao/src/main/java/com/wan/upms/dao/model/UpmsRole.java
@@ -0,0 +1,103 @@
+package com.wan.upms.dao.model;
+
+import java.io.Serializable;
+
+public class UpmsRole implements Serializable {
+    private Integer roleId;
+
+    private String name;
+
+    private String description;
+
+    private Long ctime;
+
+    private Long orders;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getCtime() {
+        return ctime;
+    }
+
+    public void setCtime(Long ctime) {
+        this.ctime = ctime;
+    }
+
+    public Long getOrders() {
+        return orders;
+    }
+
+    public void setOrders(Long orders) {
+        this.orders = orders;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", roleId=").append(roleId);
+        sb.append(", name=").append(name);
+        sb.append(", description=").append(description);
+        sb.append(", ctime=").append(ctime);
+        sb.append(", orders=").append(orders);
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        UpmsRole other = (UpmsRole) that;
+        return (this.getRoleId() == null ? other.getRoleId() == null : this.getRoleId().equals(other.getRoleId()))
+            && (this.getName() == null ? other.getName() == null : this.getName().equals(other.getName()))
+            && (this.getDescription() == null ? other.getDescription() == null : this.getDescription().equals(other.getDescription()))
+            && (this.getCtime() == null ? other.getCtime() == null : this.getCtime().equals(other.getCtime()))
+            && (this.getOrders() == null ? other.getOrders() == null : this.getOrders().equals(other.getOrders()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getRoleId() == null) ? 0 : getRoleId().hashCode());
+        result = prime * result + ((getName() == null) ? 0 : getName().hashCode());
+        result = prime * result + ((getDescription() == null) ? 0 : getDescription().hashCode());
+        result = prime * result + ((getCtime() == null) ? 0 : getCtime().hashCode());
+        result = prime * result + ((getOrders() == null) ? 0 : getOrders().hashCode());
+        return result;
+    }
+}
