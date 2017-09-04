6b4538d40d5e165a5147c18071eb6567fc5f387c
wanqinfeng <571716844@qq.com>
2017-09-04 20:16:37 +0800

    启用mybatis缓存
    Signed-off-by: wanqinfeng <571716844@qq.com>

--- cms/cms-rpc-service/src/main/java/com/wan/cms/rpc/service/CmsRpcServiceApplication.java
+++ cms/cms-rpc-service/src/main/java/com/wan/cms/rpc/service/CmsRpcServiceApplication.java
@@ -1,5 +1,7 @@
 package com.wan.cms.rpc.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -8,8 +10,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Created by w1992wishes on 2017/8/28.
  */
 public class CmsRpcServiceApplication {
+    private static Logger logger = LoggerFactory.getLogger(CmsRpcServiceApplication.class);
+
     public static void main(String[] args) {
+        logger.info(">>>>> upms-rpc-service 正在启动 <<<<<");
         new ClassPathXmlApplicationContext("classpath*:applicationContext*.xml");
-        System.out.println(">>>>> cms-rpc-service 启动完成 <<<<<");
+        logger.info(">>>>> upms-rpc-service 启动完成 <<<<<");
     }
 }
--- upms/upms-rpc-service/src/main/java/com/wan/upms/rpc/service/UpmsRpcServiceApplication.java
+++ upms/upms-rpc-service/src/main/java/com/wan/upms/rpc/service/UpmsRpcServiceApplication.java
@@ -1,13 +1,18 @@
 package com.wan.upms.rpc.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by w1992wishes on 2017/8/28.
  */
 public class UpmsRpcServiceApplication {
+    private static Logger logger = LoggerFactory.getLogger(UpmsRpcServiceApplication.class);
+
     public static void main(String[] args) {
+        logger.info(">>>>> upms-rpc-service 正在启动 <<<<<");
         new ClassPathXmlApplicationContext("classpath*:applicationContext*.xml");
-        System.out.println(">>>>> upms-rpc-service 启动完成 <<<<<");
+        logger.info(">>>>> upms-rpc-service 启动完成 <<<<<");
     }
 }
