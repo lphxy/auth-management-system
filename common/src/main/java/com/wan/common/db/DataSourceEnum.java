175d5c0d90b5881cd09f2487c0eba0b7410cb55c
wanqinfeng <571716844@qq.com>
2017-09-07 20:23:44 +0800

    单表操作自动切换数据源。如果没有指定数据源，使用默认数据源
    Signed-off-by: wanqinfeng <571716844@qq.com>

--- common/src/main/java/com/wan/common/db/DataSourceEnum.java
+++ common/src/main/java/com/wan/common/db/DataSourceEnum.java
@@ -5,9 +5,42 @@ package com.wan.common.db;
  *
  * Created by w1992wishes on 2017/8/22.
  */
-public class DataSourceEnum {
+public enum DataSourceEnum {
+
     // 主库
-    public static final String MASTER = "masterDataSource";
+    MASTER("masterDataSource", true),
     // 从库
-    public static final String SLAVE = "slaveDataSource";
+    SLAVE("slaveDataSource", false),;
+
+    // 数据源名称
+    private final String name;
+    // 是否是默认数据源
+    private final boolean master;
+
+    DataSourceEnum(String name, boolean master) {
+        this.name = name;
+        this.master = master;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isMaster() {
+        return master;
+    }
+
+    public String getDefault() {
+        String defaultDataSource = "";
+        for (DataSourceEnum dataSourceEnum : DataSourceEnum.values()) {
+            if (!"".equals(defaultDataSource)) {
+                break;
+            }
+            if (dataSourceEnum.master) {
+                defaultDataSource = dataSourceEnum.getName();
+            }
+        }
+        return defaultDataSource;
+    }
+
 }
--- common/src/main/java/com/wan/common/db/DynamicDataSource.java
+++ common/src/main/java/com/wan/common/db/DynamicDataSource.java
@@ -26,7 +26,7 @@ public class DynamicDataSource extends AbstractRoutingDataSource {
      * 设置数据源
      * @param dataSource
      */
-    public static void setDataSource(String dataSource){
+    public static void setDataSource(String dataSource) {
         contextHolder.set(dataSource);
     }
 
@@ -34,7 +34,19 @@ public class DynamicDataSource extends AbstractRoutingDataSource {
      * 获取数据源
      * @return
      */
-    public static String getDataSource(){
+    public static String getDataSource() {
+        String dataSource = contextHolder.get();
+        // 如果没有指定数据源，使用默认数据源
+        if (null == dataSource) {
+            DynamicDataSource.setDataSource(DataSourceEnum.MASTER.getDefault());
+        }
         return contextHolder.get();
+    }
+
+    /**
+     * 清除数据源
+     */
+    public static void clearDataSource() {
+        contextHolder.remove();
     }
 }
